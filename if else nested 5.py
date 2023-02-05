9fff4e3288f719e137cb3d8cc1dbed73f0cdae0a
iqbalal13 <123553063+iqbalal13@users.noreply.github.com>
2023-02-05 18:17:11 +0700

    Add files via upload

---
+++ if else nested 5.py
@@ -0,0 +1,12 @@
+i = int(input("Masukkan angka: "))
+
+if (i >= 10):
+    if (i > 10):
+        print("i adalah belasan")
+    elif (i > 19):
+        print("i adalah puluhan")
+    elif (i == 10):
+        print("i adalah puluhan")
+else:
+    (i <10)
+    print("i adalah satuan")
