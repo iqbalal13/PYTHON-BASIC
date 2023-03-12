3e1b82812804c60071adcb2625ce04a10909bf9e
iqbalal13 <123553063+iqbalal13@users.noreply.github.com>
2023-03-12 21:45:07 +0700

    Update def 8.py

--- def 8.py
+++ def 8.py
@@ -11,13 +11,3 @@ try:
 except ValueError:
     print("Invalid input. Please enter a number.")
 
-try:
-    base1 = int(input("Enter the length of the first base: "))
-    base2 = int(input("Enter the length of the second base: "))
-    height = int(input("Enter the height of the trapezium: "))
-    result = trapezium_area(base1, base2, height)
-    print("The area of the trapezium with first base", base1, "second base", base2, "and height", height, "is:", result)
-
-except ValueError:
-    print("Invalid input. Please enter a number.")
-
