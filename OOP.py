c9d4657a1cfed2a124391eeb479ef7155eb6c369
iqbalal13 <123553063+iqbalal13@users.noreply.github.com>
2023-03-15 21:02:45 +0700

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -2,9 +2,11 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        self.male = male
+        self.female = female
 
     def introduce(self):
-        print("Hello, my name is", self.name, "and I am", self.age, "years old.")
+        print("Hello, my name is", self.name, "and I am", self.age, "years old, ", self.male, "and my pronounce is he/him ", self.female, "and my pronounce is she/her")
 
-person1 = Person("John", 30)
+person1 = Person("John", 30, )
 person1.introduce()
