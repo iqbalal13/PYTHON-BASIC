ac0ad430bc531e3aae32109cf22bd8d1d771c6eb
iqbalal13 <123553063+iqbalal13@users.noreply.github.com>
2023-03-21 20:00:49 +0700

    Update SIMPLE OOP.py

--- SIMPLE OOP.py
+++ SIMPLE OOP.py
@@ -1,7 +1,21 @@
 class MyClass:
     # Class attribute
     x = 5
-
+    
     # Class method
     def my_method(self):
         print("Hello, World!")
+        
+# Creating an object of MyClass
+my_object = MyClass()
+
+# Accessing class attribute and method using object
+print(my_object.x)          # Output: 5
+my_object.my_method()       # Output: Hello, World!
+
+# Using the class as an object
+def my_function(obj):
+    obj.my_method()
+
+class_instance = MyClass()
+my_function(class_instance) 
