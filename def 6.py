da01d3c11a3025820c4f2ab7a20c407364ec5c3d
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-10-05 22:20:35 +0700

    Delete def 6.py

--- def 6.py
+++
@@ -1,9 +0,0 @@
-def square(x):
-    return x * x
-
-try:
-    num = float(input("Enter a number: "))
-    result = square(num)
-    print("The square of", num, "is:", result)
-except ValueError:
-    print("Invalid input. Please enter a number.")
