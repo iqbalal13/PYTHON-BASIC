47ddf315a570550d3e84808621260171e768610e
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-10-05 22:23:56 +0700

    Add files via upload

---
+++ PYTHON FUNCTION DEF/def 2.py
@@ -0,0 +1,6 @@
+def square(x):
+    return x * x
+
+num = int(input("Enter a number: "))
+result = square(num)
+print("The square of", num, "is:", result)
---
+++ PYTHON FUNCTION DEF/def 8.py
@@ -0,0 +1,23 @@
+def trapezium_area(base1, base2, height):
+    return 0.5 * (base1 + base2) * height
+
+try:
+    base1 = float(input("Enter the length of the first base: "))
+    base2 = float(input("Enter the length of the second base: "))
+    height = float(input("Enter the height of the trapezium: "))
+    result = trapezium_area(base1, base2, height)
+    print("The area of the trapezium with first base", base1, "second base", base2, "and height", height, "is:", result)
+
+except ValueError:
+    print("Invalid input. Please enter a number.")
+
+try:
+    base1 = int(input("Enter the length of the first base: "))
+    base2 = int(input("Enter the length of the second base: "))
+    height = int(input("Enter the height of the trapezium: "))
+    result = trapezium_area(base1, base2, height)
+    print("The area of the trapezium with first base", base1, "second base", base2, "and height", height, "is:", result)
+
+except ValueError:
+    print("Invalid input. Please enter a number.")
+
