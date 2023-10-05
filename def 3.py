5bae5a2ce00baab5dce5019ff252257dba885462
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-10-05 22:18:41 +0700

    Delete def 3.py

--- def 3.py
+++
@@ -1,7 +0,0 @@
-def triangle_area(base, height):
-    return 0.5 * base * height
-
-base = float(input("Enter the base of the triangle: "))
-height = float(input("Enter the height of the triangle: "))
-result = triangle_area(base, height)
-print("The area of the triangle with base", base, "and height", height, "is:", result)
