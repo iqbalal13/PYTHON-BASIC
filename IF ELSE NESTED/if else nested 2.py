76f6641f7d6c167d712aefd5ba33673dec6138d3
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-10-12 22:52:50 +0700

    Add files via upload

---
+++ IF ELSE NESTED/if else nested 2.py
@@ -0,0 +1,10 @@
+number = int(input("Masukkan angka: "))
+
+if (number >= 0):
+
+    if number == 0:
+        print('Number is 0')
+    else:
+        print('Number is negative')
+else:
+    print('Number is negative')
