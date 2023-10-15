b672dffb9f6d1e7806526f7c32113b6aa45f3bcd
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-10-15 16:15:36 +0700

    Delete try except 2.py

--- try except 2.py
+++
@@ -1,8 +0,0 @@
-try:
-    # code that may raise an exception
-    numerator = 10
-    denominator = 0
-    result = numerator / denominator
-except:
-    # code to handle the exception
-    print("An error occurred")
