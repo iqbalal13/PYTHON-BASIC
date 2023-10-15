8497709c23751074cffac2ae98b554b5344d60b4
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-10-15 16:16:10 +0700

    Delete try except 3.py

--- try except 3.py
+++
@@ -1,9 +0,0 @@
-try:
-    x = int(input("Enter a number: "))
-    y = int(input("Enter another number: "))
-    result = x / y
-    print("The result is:", result)
-except ValueError:
-    print("Invalid input! Please enter a valid number.")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
