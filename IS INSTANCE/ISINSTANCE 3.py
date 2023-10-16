1eff8ef12369674af7f9462f987c05c73b9853f8
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-10-16 15:51:33 +0700

    Add files via upload

---
+++ IS INSTANCE/ISINSTANCE 3.py
@@ -0,0 +1,8 @@
+x = 5
+y = "Hello, world!"
+z = [1, 2, 3]
+
+print(isinstance(x, int))    # Output: True
+print(isinstance(y, str))    # Output: True
+print(isinstance(z, list))   # Output: True
+print(isinstance(x, str))    # Output: False
