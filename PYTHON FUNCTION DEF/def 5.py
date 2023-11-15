d65a4791ff28b1e092808ab3febe703cf04699e9
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-11-15 22:50:48 +0700

    Delete PYTHON FUNCTION DEF/def 5.py

--- PYTHON FUNCTION DEF/def 5.py
+++
@@ -1,9 +0,0 @@
-def square(x):
-    return x * x
-
-try:
-    num = int(input("Enter a number: "))
-    result = square(num)
-    print("The square of", num, "is:", result)
-except ValueError:
-    print("Invalid input. Please enter a number.")
