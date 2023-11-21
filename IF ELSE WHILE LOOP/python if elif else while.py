b4c789bfa79a097e8e172db6f205d8d1e9e42dfd
iqbalal13 <muhammadiqbalalmaududii@gmail.com>
2023-11-21 21:50:10 +0700

    Add files via upload

---
+++ IF ELSE WHILE LOOP/python if elif else while.py
@@ -0,0 +1,17 @@
+while True:
+    try:
+        user_input = int(input("Enter a number (enter 0 to exit): "))
+        
+        if user_input == 0:
+            print("Exiting the program.")
+            break  # Exit the loop if the user enters 0
+            
+        if user_input > 0:
+            print("Positive number")
+        elif user_input < 0:
+            print("Negative number")
+        else:
+            print("Zero")
+            
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
